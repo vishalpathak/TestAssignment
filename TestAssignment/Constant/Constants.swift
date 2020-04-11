f7732d9be5e1229503d4d02570d82d6d58442f72
vishal <vishal.p@borqs.com>
2020-04-12 00:09:10 +0530

    Constants added -- Constants added in file for Url, common strings

--- TestAssignment/Constant/Constants.swift
+++ TestAssignment/Constant/Constants.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+//Base Url API for fetching data from server
+let BaseUrlPath = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+
+//Http Response codes
+struct HttpResponse {
+    static let ResponseSuccess = 200
+    static let ResponseNotFound = 404
+}
+
+struct AlertMessages {
+    static let AlertTitle = "Error"
+    static let MessageInfo = "Internet is not connected. Please check settings."
+    static let ButtonTitle = "OK"
+    static let CommonError = "Something went wrong please try after sometime."
+}
+
+struct DefaultStrings{
+    static let DefaultNavigationTitle = "InfoView"
+    static let DefaultTitle = "Sorry! title not available"
+    static let DefaultDescription = "Sorry! description not available"
+    static let DefaultImageNotAvailable = "Sorry! Image not available."
+}
+
+struct CommonText {
+    static let CommonTitle = "Title:"
+    static let CommonDescription = "Description:"
+    static let CommonImageInfo = "Information image:"
+}
