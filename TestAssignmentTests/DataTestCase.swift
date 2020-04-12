0a32639d0b7ff8bf1342a3964a38422ba830ff78
vishal <vishal.p@borqs.com>
2020-04-12 17:09:44 +0530

    Unit Test cases updated -- Addedd Unit test cases for API call, ViewModel -- Unit test case updated for Async Image loading -- DataTestCase class added for test API Delagate function -- Unit test added for Viewmodel

--- TestAssignmentTests/DataTestCase.swift
+++ TestAssignmentTests/DataTestCase.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import XCTest
+@testable import TestAssignment
+
+class NewTestData:XCTestCase {
+    let str = "About Canada" // Use this for passing Test Case
+    //let str = "About" // Use this for Failing Test Case
+    
+    let obj = DataInfoVM()
+    var expe = XCTestExpectation()
+    func testAPIData() -> Void {
+        expe = self.expectation(description: str)
+        obj.dataReceivedDelegate = self
+        obj.fetchDataFromApi(urlAPI: BaseUrlPath)
+        self.waitForExpectations(timeout: 10, handler: nil)
+    }
+}
+extension NewTestData: DataReceivedDelegate{
+    func didGetDataFromAPI(rowViewModel: [DataInfoViewModel]?, titleText: String?, error: Error?) {
+        if let error = error{
+            XCTFail("Failed with error\(error.localizedDescription)")
+        }
+        if let data = titleText{
+            if data == str{
+                expe.fulfill()
+            }else{
+                XCTFail("Failed with data is not equal")
+            }
+        }
+    }
+}
--- TestAssignmentTests/TestAssignmentTests.swift
+++ TestAssignmentTests/TestAssignmentTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Kingfisher
 @testable import TestAssignment
 
 class TestAssignmentTests: XCTestCase {
@@ -31,4 +32,73 @@ class TestAssignmentTests: XCTestCase {
         }
     }
 
+      //MARK:- Unit test For testing DataInfoViewModel
+        func testDataInfoModel() {
+           
+            //Model Object for passing test case
+            //let objRowInfo = RowInfo(title: "Toronto", description: "The city of Canada", imageHref: "http://www.image")
+            
+            //Model Object for failing test case
+            let objRowInfo = RowInfo(title: "Toronto", description: nil, imageHref: "http://fyimusic.ca/wp-content/uploads/2008/06/hockey-night-in-canada.thumbnail.jpg")
+            let objViewModel = DataInfoViewModel(dataInfo: objRowInfo)
+            
+            XCTAssertEqual(objRowInfo.title, objViewModel.title)
+            XCTAssertEqual(objRowInfo.description, objViewModel.description)
+            XCTAssertEqual(objRowInfo.imageHref, objViewModel.imageInfo)
+        }
+        
+        //MARK:- Unit Test For API data fecth call
+        func testApiDataFetch() {
+            //let str = "About" // Result for Failing Test Case
+            let str = "About Canada" // Result for passing Test Case
+           
+            let expectation = self.expectation(description: str)
+            
+            NetworkApiManager.sharedNetworkApiManager.getDataFromUrl(BaseUrlPath) { (dt: DataInfo?, err: Error?) in
+                if let error = err{
+                    XCTFail("Failed with error\(error.localizedDescription)")
+                }
+                if let data = dt{
+                    if data.title == str{
+                        expectation.fulfill()
+                    }else{
+                        XCTFail("Failed with data is not equal")
+                    }
+                }
+            }
+            
+            self.waitForExpectations(timeout: 10, handler: nil)
+        }
+        
+        //MARK:- For testing success and fail Image loading
+        func testImageFromUrl(){
+            
+            let imageInfo = UIImageView()
+            let expectation = self.expectation(description: CommonText.CommonImageInfo)
+            
+            var strImage: String?
+            
+            //Success Link for Image
+             strImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg"
+            
+            //Error Link for Image
+            // strImage = "http://fyimusic.ca/wp-content/uploads/2008/06/hockey-night-in-canada.thumbnail.jpg"
+            
+            //No Link
+            //strImage = nil
+            
+            guard let strLink = strImage else { return }
+            guard let url = URL(string: strLink) else { return }
+            let resource = ImageResource(downloadURL: url, cacheKey: strLink)
+            imageInfo.kf.setImage(with: resource, placeholder: UIImage(named: "placeholder_image"), options: nil, progressBlock: nil) { (result) in
+                switch result{
+                case .success( _):
+                    expectation.fulfill()
+                case .failure( _):
+                    XCTFail("Failed with image data not available")
+                }
+            }
+            self.waitForExpectations(timeout: 10, handler: nil)
+        }
+    
 }
